714319d2501c62980f58c7382a34493796d1dff2
NTS-AkshayBokhare <akshay.bokhare@neutrinotechlabs.com>
2024-07-18 16:34:18 +0530

    expense tracker_with theme

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:flutter/material.dart';
+
+var kcolorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(155, 76, 0, 255),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 5, 99, 125),
+    brightness: Brightness.dark);
+
+void main() {
+  runApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+          cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
+            ),
+          ),
+         
+          colorScheme: kDarkColorScheme),
+
+      theme: ThemeData().copyWith(
+        colorScheme: kcolorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kcolorScheme.onPrimaryContainer,
+          foregroundColor: kcolorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kcolorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kcolorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kcolorScheme.onSecondaryContainer,
+                  fontSize: 17),
+            ),
+      ),
+
+      themeMode: ThemeMode.system, //defult
+
+      home: const Expenses(),
+    ),
+  );
+}
